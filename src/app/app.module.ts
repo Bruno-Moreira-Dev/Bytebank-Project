757afcb69c77f839487dcea112a0f31afd9e1bfe
followize_bruno <bruno@followize.com.br>
2021-05-18 17:30:23 -0300

    feat: iniciando com a aplicacao
    Resolve:[master]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NovaTransferenciaModule } from './nova-transferencia/nova-transferencia.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NovaTransferenciaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nova-transferencia/nova-transferencia.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NovaTransferenciaComponent } from './nova-transferencia.component';
+
+@NgModule({
+  declarations: [NovaTransferenciaComponent],
+  imports: [
+    CommonModule,
+  ],
+  exports: [NovaTransferenciaComponent],
+})
+export class NovaTransferenciaModule { }
